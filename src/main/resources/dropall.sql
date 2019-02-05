3ee3b285de9ccd8f7bf5734bff7860ef099f7f45
James Flynn <jflynn@wemanity.com>
2019-02-05 16:20:36 +0100

    Adding database

---
+++ src/main/resources/dropall.sql
@@ -0,0 +1,11 @@
+
+drop table ACCOUNTS;
+drop table LEDGER;
+drop table ADDRESSES;
+drop table CUSTOMERS;
+
+drop sequence LEDGER_SEQUENCE;
+drop sequence CUSTOMER_SEQUENCE;
+drop sequence  ACCOUNT_SEQUENCE;
+
+commit;
