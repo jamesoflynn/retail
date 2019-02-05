88ed43984d3f98ea10517a4746496404bf00b2e0
jamesflynn <jflynn@wemanity.com>
2019-02-05 22:53:10 +0100

    more stuff

--- src/main/java/wemanibank/retail/CustomerManager.java
+++ src/main/java/wemanibank/retail/CustomerManager.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Component;
 
@@ -22,14 +23,23 @@ public class CustomerManager {
 		entityManager = emf.createEntityManager();
 	}
 
+	private static final String CUSTOMER_SELECT = "select cust from CUSTOMERS c where c.userName=(:userName) and c.secret=(:secret)";
+
 	public Customer customerLogin(Customer customer) {
-		customer = entityManager.find(Customer.class, customer);
+		try {
+			TypedQuery<Customer> query = entityManager.createQuery(CUSTOMER_SELECT, Customer.class);
+			query.setParameter("userName", customer.getUserName());
+			query.setParameter("secret", customer.getSecret());
+		}
+		catch (Exception e) {
+			
+		}
 		return customer;
 	}
 
 	public Account deposit(Account account, double amount) throws AccountException {
 		try {
-			account = entityManager.find(Account.class, account);
+//			account = entityManager
 			account.deposit(amount);
 		}
 		catch(Exception e) {
--- src/main/java/wemanibank/retail/entities/Account.java
+++ src/main/java/wemanibank/retail/entities/Account.java
@@ -2,14 +2,21 @@ package wemanibank.retail.entities;
 
 import java.util.Calendar;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import wemanibank.retail.AccountException;
 
 @Entity(name="ACCOUNTS")
 public class Account {
+	@Id
+	private Integer accountNumber;
+	@Column(name="BALANCE")
 	private double balance;
+	@Column(name="ASOFDATE")
 	private Calendar asOfDate;
+	
 
 	public void deposit(double amount) throws AccountException {
 		
