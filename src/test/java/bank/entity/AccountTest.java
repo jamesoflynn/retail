c8f057bf22cb42666cae69dd1504d0400030038c
James Oisin Flynn <den.galna.kon@hotmail.com>
2019-02-21 12:46:07 +0100

    cleanup

--- src/test/java/bank/entity/AccountTest.java
+++ src/test/java/bank/entity/AccountTest.java
@@ -8,15 +8,10 @@ import junit.framework.TestCase;
 
 public class AccountTest extends TestCase {
 
-	Account account;
-
-	@Before
-	public void setup() {
-		account = new Account();
-	}
 
 	@Test // Test that we can deposit money into an Account.
 	public void testAccountDeposit() {
+		Account account = new Account();
 		double amount = account.deposit(10.0);
 		Assert.assertTrue(amount == 10.0);
 	}
