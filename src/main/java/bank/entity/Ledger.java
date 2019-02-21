ecc8709dd4ff10ee9a54a0498d347c12207dec17
James Oisin Flynn <den.galna.kon@hotmail.com>
2019-02-21 14:15:44 +0100

    Adding the beginnings of US3

---
+++ src/main/java/bank/entity/Ledger.java
@@ -0,0 +1,20 @@
+package bank.entity;
+
+import java.util.Calendar;
+
+public class Ledger {
+
+	private Calendar date;
+	private double deposit;
+	private double withdrawal;
+
+	public Ledger() {}
+
+	public double deposit(double amount) {
+		return 0.0;
+	}
+
+	public double withdraw(double mount) {
+		return 0.0;
+	}
+}
---
+++ src/test/java/bank/entity/LedgerTest.java
@@ -0,0 +1,9 @@
+package bank.entity;
+
+import junit.framework.TestCase;
+
+public class LedgerTest extends TestCase {
+
+
+	
+}
