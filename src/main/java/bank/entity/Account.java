a8b21119999bf228b67893cfde1e5d9c38d3a371
James Oisin Flynn <den.galna.kon@hotmail.com>
2019-03-06 19:44:37 +0100

    Refactor

--- src/main/java/bank/entity/Account.java
+++ src/main/java/bank/entity/Account.java
@@ -1,16 +1,25 @@
 package bank.entity;
 
+import java.time.LocalDateTime;
+
 public class Account {
 
 	private double balance;
+	private Ledger ledger;
+	private LocalDateTime asOfDate;
 
 	public Account() {
 		balance = 0.0d;
+		ledger = new Ledger();
 	}
 
 	public double deposit(double amount) {
 		if (amount > 0) {
-			balance += amount;
+			double added = ledger.deposit(amount);
+			if (added == amount) {
+				balance += added;
+				asOfDate = LocalDateTime.now();
+			}
 		}
 		return balance;
 	}
@@ -18,8 +27,9 @@ public class Account {
 	public double withdraw(double amount) {
 		double withdrawn = 0.0d;
 		if (amount <= balance) {
-			balance -= amount;
-			withdrawn = amount;
+			withdrawn = ledger.withdraw(amount);
+			balance -= withdrawn;
+			asOfDate = LocalDateTime.now();
 		}
 		return withdrawn;
 	}
