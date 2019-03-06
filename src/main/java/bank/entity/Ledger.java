ea097d690a874ec5506113cd10368fec5862c737
James Oisin Flynn <den.galna.kon@hotmail.com>
2019-03-06 19:30:10 +0100

    Added implementation of first test.

--- src/main/java/bank/entity/Ledger.java
+++ src/main/java/bank/entity/Ledger.java
@@ -1,20 +1,38 @@
 package bank.entity;
 
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ledger {
 
-	private Calendar date;
-	private double deposit;
-	private double withdrawal;
+	class Entry {
+		private LocalDateTime date;
+		private double deposit;
+		private double withdrawal;
 
-	public Ledger() {}
-
-	public double deposit(double amount) {
-		return 0.0;
+		Entry() {
+			this.date = LocalDateTime.now();
+		}
 	}
 
-	public double withdraw(double mount) {
+	private List<Entry> entries;
+
+	public Ledger() {
+		this.entries = new ArrayList<Entry>();
+	}
+
+	public double deposit(double amount) {
+		Entry entry = null;
+		if (amount > 0) {
+			entry = new Entry();
+			entry.deposit = amount;
+			entries.add(entry);
+		}
+		return entry != null ? entry.deposit : 0.0;
+	}
+
+	public double withdraw(double amount) {
 		return 0.0;
 	}
 }
