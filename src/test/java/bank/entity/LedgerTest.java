ebe2d226d51755ca4069c09e373bb0e79362512d
James Oisin Flynn <den.galna.kon@hotmail.com>
2019-03-06 19:15:20 +0100

    added failing test, copied from account test.

--- src/test/java/bank/entity/LedgerTest.java
+++ src/test/java/bank/entity/LedgerTest.java
@@ -1,9 +1,17 @@
 package bank.entity;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class LedgerTest extends TestCase {
 
 
-	
+	@Test // Refactor of AccountTest
+	public void testDeposit() {
+		Ledger ledger = new Ledger();
+		double amount = ledger.deposit(10.0);
+		Assert.assertTrue(amount == 10.0);
+	}
 }
