d3e98fd7520e4ca78435e74f7b2e9392b7e09688
James Oisin Flynn <den.galna.kon@hotmail.com>
2019-03-12 18:33:23 +0100

    Added code to create a new customer.

--- src/main/java/bank/entity/Bank.java
+++ src/main/java/bank/entity/Bank.java
@@ -1,8 +1,25 @@
 package bank.entity;
 
+import java.util.HashMap;
+
 public class Bank {
+	private HashMap<String, Customer> customers;
+	private static Bank self;
+
+	static {
+		self = new Bank();
+	}
+
+	private Bank() {
+		customers = new HashMap<>();
+	}
 
 	public static Customer createNewCustomer(String name) {
-		return null;
+		Customer customer = null;
+		if (!self.customers.containsKey(name)) {
+			customer = new Customer(name);
+			self.customers.put(name, customer);
+		}
+		return customer;
 	}
 }
